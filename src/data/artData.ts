8ee773681f72e56e4431a0e0dafec22d73050c33
Grace Huang <gracefh@mit.edu>
2025-12-24 00:40:22 -0800

    add more art information

--- src/data/artData.ts
+++ src/data/artData.ts
@@ -1,7 +1,12 @@
 export const images = [
-    "image/art/cat-hearts.png",
+  "image/art/holiday-cards.JPG",
+  "image/art/taiwan-night-markets.PNG",
+  "image/art/boba-cat-pins.JPG",
+  "image/art/cat-hearts.png",
   "image/art/karl-the-fog.png",
   "image/art/boba-panda.png",
   "image/art/2024-3-23-fly-no-border.png",
+  "image/art/synco-audition-poster-2023.png",
   "image/project-previews/koi-cards-preview.png",
+  "image/art/synco-concert-poster-2021.png",
 ];
--- src/pages/art.tsx
+++ src/pages/art.tsx
@@ -1,18 +1,45 @@
 import { ImageGallery } from "../layout/imageGallery";
-import {images} from "../data/artData"
+import { images } from "../data/artData";
 
 export const Art = () => {
   return (
     <main>
-        <section>
+      <section>
         <div className="section-header">
           <h2 className="text-pink header-lg">ART</h2>
-          <div className="subtitle">digital arts, crafts, and other creative pursuits</div>
         </div>
-        <ImageGallery
-            images={images}
-            />
-        </section>
+      </section>
+      <article>
+        <p>
+          One of my hobbies outside of work is art! I remember doodling in the
+          pages of my notebooks at school all the way back in 4th grade, and
+          I've continued doodling my way through life. In the past couple of
+          years, I've been able to pursue a variety of different projects: I
+          designed a deck of koi playing cards in 2022, tried making a poster on
+          Adobe Illustrator every day for a month in 2024, and did my first
+          craft market on Halloween of 2025. This is a small collection of some
+          of the art I've been able to create, but you can find more information
+          about my art on{" "}
+          <a
+            href="https://www.butterandboba.com/"
+            target="_blank"
+            className="text-link"
+          >
+            butterandboba.com
+          </a>{" "}
+          or on instagram{" "}
+          <a
+            href="https://www.instagram.com/butter.and.boba/"
+            className="text-link"
+            target="_blank"
+          >
+            @butter.and.boba
+          </a>
+          !
+        </p>
+
+        <ImageGallery images={images} />
+      </article>
     </main>
   );
 };
